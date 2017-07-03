0ed9e30ac04bb6895fc255fa4b7429406796f25f
Larixs <lairuixin@gstianfu.com>
2017-07-03 18:34:21 +0800

    修改了build出的文件路径

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -72,7 +72,7 @@ const entries = webpackConfig.entry;
 Object.keys(entries).forEach(function (name){
     webpackConfig.plugins.push(
         new HtmlWebpackPlugin({
-            filename: path.resolve(distPath, name, "index.html"),
+            filename: path.resolve(distPath, name+".html"),
             template: path.resolve(srcPath, 'index_build.html'),
             inject: true,
             chunks:[name], //让各自文件的html引用各自的js，不会把所有的js文件都用上
