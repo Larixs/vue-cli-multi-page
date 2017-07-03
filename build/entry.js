ab752456aae868ecb519396eec51c44fff1ca67c
Larixs <lairuixin@gstianfu.com>
2017-07-03 15:59:06 +0800

    从命令行读取需要编译的目录

--- build/entry.js
+++ build/entry.js
@@ -1,19 +1,18 @@
 const path = require('path');
 const fs = require('fs');
-const srcPath =  path.join(__dirname, '../src');
-//configEntries为自己设定的编译项目名称
-let configEntries = [];
-const allEntries = fs.readdirSync(srcPath);
+const srcPath = path.join(__dirname, '../src');
+//commandEntries为自己设定的编译项目名称，从node命令中读取
+const commandEntries = process.argv.slice(2);
 //如果configEntries为空，默认编译所有子项目
-configEntries = configEntries.length ? configEntries : allEntries;
-console.log("configEntry", configEntries);
-module.exports = configEntries.reduce((entries, dir) => {
-        //读取src下的所有文件夹
-        const fullDir = path.join(srcPath, dir);
-        const entry = path.resolve(fullDir, 'app.js');
-        if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry))
-        {
-            entries[dir] = entry;
-        }
-        return entries
-    }, {});
+const allEntries = fs.readdirSync(srcPath);
+
+const configEntries = commandEntries.length ? commandEntries : allEntries;
+module.exports = configEntries.reduce((entries, dir) =>{
+    //读取src下的所有文件夹
+    const fullDir = path.join(srcPath, dir);
+    const entry = path.resolve(fullDir, 'app.js');
+    if ( fs.statSync(fullDir).isDirectory() && fs.existsSync(entry) ) {
+        entries[dir] = entry;
+    }
+    return entries
+}, {});
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 1. npm install
 2. npm run dev / npm run build
 
-src里除了fonts和img文件夹，其余文件夹均为子项目。每个子项目可以单独打包编译，只需在./build/entry.js里对configEntries赋初值即可。如果不赋初值，那么默认打包所有子项目文件。
+src里除了fonts和img文件夹，其余文件夹均为子项目。每个子项目可以单独打包编译。只需npm run build + 项目名称 即可。如果不赋初值，那么默认打包所有子项目文件。
