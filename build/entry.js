04eaa40e0245900e4d7779b8494f3900e038836d
Larixs <lairuixin@gstianfu.com>
2017-07-03 12:11:35 +0800

    修改readme

--- build/entry.js
+++ build/entry.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const srcPath =  path.join(__dirname, '../src');
+//configEntries为自己设定的编译项目名称
 let configEntries = [];
 const allEntries = fs.readdirSync(srcPath);
 //如果configEntries为空，默认编译所有子项目
